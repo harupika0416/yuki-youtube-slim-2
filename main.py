91147ec9238b57b88836da65e12d40d16dcdf91a
harupika0416 <167415535+harupika0416@users.noreply.github.com>
2024-10-02 17:02:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ from typing import Union
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.mount("/css", StaticFiles(directory="./css"), name="static")
-app.mount("/harupika", StaticFiles(directory="./blog", html=True), name="static")
+app.mount("/word", StaticFiles(directory="./blog", html=True), name="static")
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 from fastapi.templating import Jinja2Templates
@@ -197,7 +197,7 @@ def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
         response.set_cookie("スプラチャージャー","True",max_age=60 * 60 * 24 * 7)
         return template("home.html",{"request": request})
     print(check_cokie(スプラチャージャー))
-    return redirect("/hatupika")
+    return redirect("/word")
 
 @app.get('/watch', response_class=HTMLResponse)
 def video(v:str,response: Response,request: Request,スプラチャージャー: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
